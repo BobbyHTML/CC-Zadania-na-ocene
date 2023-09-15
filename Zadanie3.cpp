af9c329d16e7b67776cabbb8bad14f6248ce2974
BobbyHTML <102832957+BobbyHTML@users.noreply.github.com>
2023-09-15 13:57:52 +0200

    Create Zadanie3.cpp

---
+++ Zadanie3.cpp
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int rozmiar = 10;
+    
+    for (int i = 0; i < rozmiar; ++i){
+        for (int j = 0; j < rozmiar; ++j){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
