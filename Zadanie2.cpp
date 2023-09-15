49c693ec18bb2a8fef21728593adf1f870bf8fc4
BobbyHTML <102832957+BobbyHTML@users.noreply.github.com>
2023-09-15 13:57:28 +0200

    Create Zadanie2.cpp

---
+++ Zadanie2.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+     char alfabet[] = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+   
+   for ( int i = 0; i < sizeof(alfabet); ++i){
+       cout << alfabet[i] << " ";
+   }
+}
+    // Write 
